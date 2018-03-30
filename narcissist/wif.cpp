1a51b9663977c5f9b50a15b8436877591b094bbf
Matthew Incognito <matthew@incognito.host>
2018-03-30 21:32:20 +0100

    Add WIF export for private keys

---
+++ narcissist/wif.cpp
@@ -0,0 +1,31 @@
+#include <cryptopp/secblock.h>
+#include <cryptopp/sha.h>
+
+#include <narcissist/base58.h>
+#include <narcissist/wif.hpp>
+
+static CryptoPP::SHA256 sha256;
+
+static inline void sha256d(byte *out, const byte *in, size_t in_length)
+{
+	// sha256(sha256(x))
+
+	sha256.CalculateDigest(out, in, in_length);
+	sha256.CalculateDigest(out, out, CryptoPP::SHA256::DIGESTSIZE);
+}
+
+void Narcissist::ecdsa_to_wif(char *base58, byte prefix, const CryptoPP::SecByteBlock key)
+{
+	// 1 byte prefix + key + 0x01 + 4 bytes of checksum
+	CryptoPP::SecByteBlock wif(key.size() + 6);
+
+	wif.data()[0] = prefix;
+	memcpy(wif.data() + 1, key, key.size());
+	wif.data()[key.size() + 1] = 0x01;
+
+	byte checksum[CryptoPP::SHA256::DIGESTSIZE];
+	sha256d(checksum, wif, key.size() + 2);
+	memcpy(wif.data() + key.size() + 2, checksum, 4);
+
+	base58enc(base58, nullptr, wif.data(), wif.size());
+}
